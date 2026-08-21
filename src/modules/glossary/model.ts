593ff030981378ced871a5fd1152fc85dff1134b
tuplePot <joshtrilenggu@proton.me>
2026-08-21 20:02:56 +0700

    chore:test only

--- src/modules/glossary/model.ts
+++ src/modules/glossary/model.ts
@@ -23,7 +23,7 @@ export const glossaryUpdate = t.Partial(
     confidence: confidenceEnum,
   })
 )
-// test log
+// test tokenn test generate
 export const glossaryQuery = t.Object({
   search: t.Optional(t.String()),
   category: t.Optional(t.String({ maxLength: 100 })),
